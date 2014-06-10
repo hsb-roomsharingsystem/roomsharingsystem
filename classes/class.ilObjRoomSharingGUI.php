a44b781408db54a73f60713295bfb062d4913a0d
Thomas Matern <tmb@live.de>
2014-06-10 10:28:32 +0200

    Holen/Setzen der pool_id korregiert

--- classes/class.ilObjRoomSharingGUI.php
+++ classes/class.ilObjRoomSharingGUI.php
@@ -22,6 +22,7 @@ include_once('Services/Form/classes/class.ilPropertyFormGUI.php');
  *
  * @ilCtrl_Calls ilObjRoomSharingGUI: ilPermissionGUI, ilInfoScreenGUI, ilObjectCopyGUI, ilCommonActionDispatcherGUI, ilRoomSharingSearchGUI
  * @ilCtrl_Calls ilObjRoomSharingGUI: ilRoomSharingAppointmentsGUI, ilRoomsharingRoomsGUI, ilRoomsharingFloorplansGUI, ilPublicUserProfileGUI
+ * @ilCtrl_Calls ilObjRoomSharingGUI: ilRoomsharingRoomGUI
  * @ilCtrl_isCalledBy ilObjRoomSharingGUI: ilRepositoryGUI, ilAdministrationGUI, ilObjPluginDispatchGUI 
  *
  */
@@ -39,7 +40,7 @@ class ilObjRoomSharingGUI extends ilObjectPluginGUI
     protected function afterConstructor()
     {
         // Set pool id.
-        $this->pool_id = $this->object->object_id;
+        $this->pool_id = $this->obj_id;
     }
 
     /**
@@ -211,7 +212,7 @@ class ilObjRoomSharingGUI extends ilObjectPluginGUI
         $this->tabs_gui->addTab("search", $this->lng->txt("search"), $this->ctrl->getLinkTargetByClass('ilroomsharingsearchgui', "showSearchQuick"));
 
         // Rooms
-        $this->tabs_gui->addTab("rooms", $this->txt("rooms"), $this->ctrl->getLinkTargetByClass('ilroomsharingroomsgui', "showRooms"));
+        $this->tabs_gui->addTab("rooms", $this->txt("rooms"), $this->ctrl->getLinkTargetByClass('ilroomsharingroomgui', "showRoom"));
 
         // Floorplans
         $this->tabs_gui->addTab("floor_plans", $this->txt("room_floor_plans"), $this->ctrl->getLinkTargetByClass("ilroomsharingfloorplansgui", "render"));
