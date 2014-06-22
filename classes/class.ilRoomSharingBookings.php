4026f724c384f9ece10f7553c62e4051159c3f00
Tim Röhrig <troehrig@stud.hs-bremen.de>
2014-06-22 20:57:18 +0200

    #310 Review SQL - Booking & Room Zweite Formatierung (halb manuell)

--- classes/class.ilRoomSharingBookings.php
+++ classes/class.ilRoomSharingBookings.php
@@ -13,7 +13,7 @@ class ilRoomSharingBookings
 	/**
 	 * constructor ilRoomSharingBookings
 	 *
-	 * @param type $pool_id        	
+	 * @param integer $pool_id        	
 	 */
 	function __construct($pool_id = 1)
 	{
@@ -33,46 +33,52 @@ class ilRoomSharingBookings
 	{
 		global $ilDB, $ilUser, $ilCtrl, $lng;
 		
-		if (! empty( $a_booking_id ) && is_numeric( $a_booking_id ))
+		if (! empty($a_booking_id) && is_numeric($a_booking_id))
 		{
-			$set = $ilDB->query( 'SELECT seq_id, user_id' . ' FROM rep_robj_xrs_bookings' . ' WHERE id = ' . $ilDB->quote( $a_booking_id, 'integer' ) );
-			$row = $ilDB->fetchAssoc( $set );
+			$set = $ilDB->query('SELECT seq_id, user_id'.' FROM rep_robj_xrs_bookings'.
+					' WHERE id = '.$ilDB->quote($a_booking_id, 'integer'));
+			$row = $ilDB->fetchAssoc($set);
 			
 			// Check if there is a result (so the booking with the ID exists)
-			if ($ilDB->numRows( $set ) > 0)
+			if ($ilDB->numRows($set) > 0)
 			{
 				// Check if the current user is the author of the booking
 				if ($row ['user_id'] == $ilUser->getId())
 				{
 					// Check whether only the specific booking should be deleted
-					if (! $a_seq || $row ['seq_id'] == NULL || ! is_numeric( $row ['seq_id'] ))
+					if (! $a_seq || $row ['seq_id'] == NULL || ! is_numeric($row ['seq_id']))
 					{
-						$ilDB->query( 'DELETE FROM rep_robj_xrs_bookings' . ' WHERE id = ' . $ilDB->quote( $a_booking_id, 'integer' ) );
-						$ilDB->query( 'DELETE FROM rep_robj_xrs_book_user' . ' WHERE booking_id = ' . $ilDB->quote( $a_booking_id, 'integer' ) );
-						ilUtil::sendSuccess( $lng->txt( 'rep_robj_xrs_booking_deleted' ), true );
-					} 					// else delete every booking in the sequence
-					else
+						$ilDB->query('DELETE FROM rep_robj_xrs_bookings'.
+								' WHERE id = '.$ilDB->quote($a_booking_id, 'integer'));
+						$ilDB->query('DELETE FROM rep_robj_xrs_book_user'.
+								' WHERE booking_id = '.$ilDB->quote($a_booking_id, 'integer'));
+						ilUtil::sendSuccess($lng->txt('rep_robj_xrs_booking_deleted'), true);
+					} // else
 					{
 						// Get every booking which is in the specific sequence
-						$seq_set = $ilDB->query( 'SELECT id FROM rep_robj_xrs_bookings' . ' WHERE seq = ' . $ilDB->quote( $row ['seq'], 'integer' ) . ' AND pool_id = ' . $ilDB->quote( $pool_id, 'integer' ) );
-						while ( $seq_row = $ilDB->fetchAssoc( $seq_set ) )
+						$seq_set = $ilDB->query('SELECT id FROM rep_robj_xrs_bookings'.
+								' WHERE seq = '.$ilDB->quote($row ['seq'], 'integer').
+								' AND pool_id = '.$ilDB->quote($pool_id, 'integer'));
+						while ($seq_row = $ilDB->fetchAssoc($seq_set))
 						{
-							$ilDB->query( 'DELETE FROM rep_robj_xrs_bookings' . ' WHERE id = ' . $ilDB->quote( $a_booking_id, 'integer' ) );
-							$ilDB->query( 'DELETE FROM rep_robj_xrs_book_user' . ' WHERE booking_id = ' . $ilDB->quote( $a_booking_id, 'integer' ) );
-							ilUtil::sendSuccess( $lng->txt( 'rep_robj_xrs_booking_sequence_deleted' ), true );
+							$ilDB->query('DELETE FROM rep_robj_xrs_bookings'.
+									' WHERE id = '.$ilDB->quote($a_booking_id, 'integer'));
+							$ilDB->query('DELETE FROM rep_robj_xrs_book_user'.
+									' WHERE booking_id = '.$ilDB->quote($a_booking_id, 'integer'));
+							ilUtil::sendSuccess($lng->txt('rep_robj_xrs_booking_sequence_deleted'), true);
 						}
 					}
 				} else
 				{
-					ilUtil::sendFailure( $lng->txt( "rep_robj_xrs_no_delete_permission" ), true );
+					ilUtil::sendFailure($lng->txt("rep_robj_xrs_no_delete_permission"), true);
 				}
 			} else
 			{
-				ilUtil::sendFailure( $lng->txt( "rep_robj_xrs_booking_doesnt_exist" ), true );
+				ilUtil::sendFailure($lng->txt("rep_robj_xrs_booking_doesnt_exist"), true);
 			}
 		} else
 		{
-			ilUtil::sendFailure( $lng->txt( "rep_robj_xrs_no_id_submitted" ), true );
+			ilUtil::sendFailure($lng->txt("rep_robj_xrs_no_id_submitted"), true);
 		}
 	}
 	
@@ -86,36 +92,47 @@ class ilRoomSharingBookings
 	{
 		global $ilDB, $ilUser, $lng;
 		
-		$set = $ilDB->query( 'SELECT *' . ' FROM rep_robj_xrs_bookings' . ' WHERE pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) . ' AND user_id = ' . $ilDB->quote( $ilUser->getId(), 'integer' ) . ' AND (date_from >= "' . date( 'Y-m-d H:i:s' ) . '"' . ' OR date_to >= "' . date( 'Y-m-d H:i:s' ) . '")' . ' ORDER BY date_from ASC' );
+		$set = $ilDB->query('SELECT *'.' FROM rep_robj_xrs_bookings'.
+				' WHERE pool_id = '.$ilDB->quote($this->pool_id, 'integer').
+				' AND user_id = '.$ilDB->quote($ilUser->getId(), 'integer').
+				' AND (date_from >= "'.date('Y-m-d H:i:s').'"'.
+				' OR date_to >= "'.date('Y-m-d H:i:s').'")'.
+				' ORDER BY date_from ASC');
 		$res = array ();
-		while ( $row = $ilDB->fetchAssoc( $set ) )
+		while ($row = $ilDB->fetchAssoc($set))
 		{
 			$one_booking = array ();
 			// Is it a recurring appointment?
-			if (is_numeric( $row ['seq_id'] ))
+			if (is_numeric($row ['seq_id']))
 			{
 				$one_booking ['recurrence'] = true;
 			} else
 			{
-				$date_from = DateTime::createFromFormat( "Y-m-d H:i:s", $row ['date_from'] );
-				$date_to = DateTime::createFromFormat( "Y-m-d H:i:s", $row ['date_to'] );
-				$date = $date_from->format( 'd' ) . '.' . ' ' . $lng->txt( 'month_' . $date_from->format( 'm' ) . '_short' ) . ' ' . $date_from->format( 'Y' ) . ',' . ' ' . $date_from->format( 'H:i' );
+				$date_from = DateTime::createFromFormat("Y-m-d H:i:s", $row ['date_from']);
+				$date_to = DateTime::createFromFormat("Y-m-d H:i:s", $row ['date_to']);
+				$date = $date_from->format('d').'. '.$lng->txt('month_'.
+						$date_from->format('m').'_short').' '.
+						$date_from->format('Y').', '.
+						$date_from->format('H:i');
 				$date .= " - ";
 				
 				// Check whether the date_from differs from the date_to
-				if ($date_from->format( 'dmY' ) !== $date_to->format( 'dmY' ))
+				if ($date_from->format('dmY') !== $date_to->format('dmY'))
 				{
-					$date .= '<br>' . $date_to->format( 'd' ) . '.' . ' ' . $lng->txt( 'month_' . $date_to->format( 'm' ) . '_short' ) . ' ' . $date_to->format( 'Y' ) . ', ';
+					$date .= '<br>'.$date_to->format('d').'. '.
+					$lng->txt('month_'.$date_to->format('m').'_short').' '.
+					$date_to->format('Y').', ';
 				}
 				
-				$date .= $date_to->format( 'H:i' );
+				$date .= $date_to->format('H:i');
 			}
 			
 			$one_booking ['date'] = $date;
 			
 			// Get the name of the booked room
-			$roomSet = $ilDB->query( 'SELECT name FROM rep_robj_xrs_rooms' . ' WHERE id = ' . $ilDB->quote( $row ['room_id'], 'integer' ) );
-			$roomRow = $ilDB->fetchAssoc( $roomSet );
+			$roomSet = $ilDB->query('SELECT name FROM rep_robj_xrs_rooms'.
+					' WHERE id = '.$ilDB->quote($row ['room_id'], 'integer'));
+			$roomRow = $ilDB->fetchAssoc($roomSet);
 			$one_booking ['room'] = $roomRow ['name'];
 			$one_booking ['room_id'] = $row ['room_id'];
 			
@@ -123,13 +140,18 @@ class ilRoomSharingBookings
 			$participants_ids = array ();
 			
 			// Get the participants
-			$participantSet = $ilDB->query( 'SELECT users.firstname AS firstname,' . ' users.lastname AS lastname, users.login AS login,' . ' users.usr_id AS id FROM rep_robj_xrs_book_user' . ' LEFT JOIN usr_data AS users ON users.usr_id = rep_robj_xrs_book_user.user_id' . ' WHERE booking_id = ' . $ilDB->quote( $row ['id'], 'integer' ) . ' ORDER BY users.lastname, users.firstname ASC' );
-			while ( $participantRow = $ilDB->fetchAssoc( $participantSet ) )
+			$participantSet = $ilDB->query('SELECT users.firstname AS firstname,'.
+					' users.lastname AS lastname, users.login AS login,'.
+					' users.usr_id AS id FROM rep_robj_xrs_book_user'.
+					' LEFT JOIN usr_data AS users ON users.usr_id = rep_robj_xrs_book_user.user_id'.
+					' WHERE booking_id = '.$ilDB->quote($row ['id'], 'integer').
+					' ORDER BY users.lastname, users.firstname ASC');
+			while ($participantRow = $ilDB->fetchAssoc($participantSet))
 			{
 				// Check if the user has a firstname and lastname
-				if (empty( $userRow ['firstname'] ) || empty( $userRow ['lastname'] ))
+				if (empty($userRow ['firstname']) || empty($userRow ['lastname']))
 				{
-					$participants [] = $participantRow ['firstname'] . ' ' . $participantRow ['lastname'];
+					$participants [] = $participantRow ['firstname'].' '.$participantRow ['lastname'];
 				} 				// ...if not, use the username
 				else
 				{
@@ -142,8 +164,12 @@ class ilRoomSharingBookings
 			$one_booking ['subject'] = $row ['subject'];
 			
 			// Get variable attributes of a booking
-			$attributesSet = $ilDB->query( 'SELECT value, attr.name AS name' . ' FROM rep_robj_xrs_book_attr' . ' LEFT JOIN rep_robj_xrs_battr AS attr' . ' ON attr.id = rep_robj_xrs_book_attr.attr_id' . ' WHERE booking_id = ' . $ilDB->quote( $row ['id'], 'integer' ) );
-			while ( $attributesRow = $ilDB->fetchAssoc( $attributesSet ) )
+			$attributesSet = $ilDB->query('SELECT value, attr.name AS name'.
+					' FROM rep_robj_xrs_book_attr'.
+					' LEFT JOIN rep_robj_xrs_battr AS attr'.
+					' ON attr.id = rep_robj_xrs_book_attr.attr_id'.
+					' WHERE booking_id = '.$ilDB->quote($row ['id'], 'integer'));
+			while ($attributesRow = $ilDB->fetchAssoc($attributesSet))
 			{
 				$one_booking [$attributesRow ['name']] = $attributesRow ['value'];
 			}
@@ -187,13 +213,16 @@ class ilRoomSharingBookings
 	/**
 	 * Returns all the additional information that can be displayed in the
 	 * bookings table.
+	 *
+	 * @return array $cols
 	 */
 	public function getAdditionalBookingInfos()
 	{
 		global $ilDB;
 		$cols = array ();
-		$attributesSet = $ilDB->query( 'SELECT *' . ' FROM rep_robj_xrs_battr' . ' WHERE pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) );
-		while ( $attributesRow = $ilDB->fetchAssoc( $attributesSet ) )
+		$attributesSet = $ilDB->query('SELECT *'.' FROM rep_robj_xrs_battr'.
+				' WHERE pool_id = '.$ilDB->quote($this->pool_id, 'integer'));
+		while ($attributesRow = $ilDB->fetchAssoc($attributesSet))
 		{
 			$cols [$attributesRow ['name']] = array (
 					"txt" => $attributesRow ['name'],
@@ -217,12 +246,23 @@ class ilRoomSharingBookings
 		
 		return $cols;
 	}
+	/**
+	 * Set the poolID of bookings
+	 *
+	 * @param integer $pool_id
+	 *        	poolID
+	 */
 	public function setPoolId($pool_id)
 	{
 		$this->pool_id = $pool_id;
 	}
+	/**
+	 * Get the PoolID of bookings
+	 *
+	 * @return integer PoolID
+	 */
 	public function getPoolId()
 	{
-		return $this->pool_id;
+		return (int) $this->pool_id;
 	}
 }
--- classes/class.ilRoomSharingRooms.php
+++ classes/class.ilRoomSharingRooms.php
@@ -44,7 +44,7 @@ class ilRoomSharingRooms
 			echo "<br />";
 			echo "Entered getList() with filter-array:";
 			echo "<br />";
-			print_r( $filter );
+			print_r($filter);
 			echo "<br />";
 		}
 		
@@ -56,20 +56,23 @@ class ilRoomSharingRooms
 		$roomsMatchingAttributeFilters = array ();
 		if ($filter ['attributes'])
 		{
-			foreach( $filter ['attributes'] as $key => $value )
+			foreach ($filter ['attributes'] as $key => $value)
 			{
 				$count = $count + 1;
-				$q = 'SELECT room_id FROM rep_robj_xrs_room_attr ra LEFT JOIN rep_robj_xrs_rattr attr ON ra.att_id = attr.id WHERE name = ' . $ilDB->quote( $key, 'text' ) . ' AND count >= ' . $ilDB->quote( $value, 'integer' ) . ' AND pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) . ' ';
+				$q = 'SELECT room_id FROM rep_robj_xrs_room_attr ra LEFT JOIN rep_robj_xrs_rattr attr ON ra.att_id = attr.id WHERE name = '.
+				$ilDB->quote($key, 'text').
+				' AND count >= '.$ilDB->quote($value, 'integer').
+				' AND pool_id = '.$ilDB->quote($this->pool_id, 'integer').' ';
 				if ($debug)
 				{
 					echo "<br />";
 					echo $q;
 					echo "<br />";
 				}
-				$resAttr = $ilDB->query( $q );
-				while ( $row = $ilDB->fetchAssoc( $resAttr ) )
+				$resAttr = $ilDB->query($q);
+				while ($row = $ilDB->fetchAssoc($resAttr))
 				{
-					if (! array_key_exists( $row ['room_id'], $roomsWithAttrib ))
+					if (! array_key_exists($row ['room_id'], $roomsWithAttrib))
 					{
 						$roomsWithAttrib [$row ['room_id']] = 1;
 					} else
@@ -81,10 +84,10 @@ class ilRoomSharingRooms
 			if ($debug)
 			{
 				echo "<br />";
-				echo "count: " . $count;
+				echo "count: ".$count;
 				echo "<br />";
 			}
-			foreach( $roomsWithAttrib as $key => $value )
+			foreach ($roomsWithAttrib as $key => $value)
 			{
 				if ($value == $count)
 				{
@@ -94,8 +97,8 @@ class ilRoomSharingRooms
 		} else
 		{ // when no filter set, get all
 			$query = 'SELECT id FROM rep_robj_xrs_rooms';
-			$resRoomIds = $ilDB->query( $query );
-			while ( $row = $ilDB->fetchAssoc( $resRoomIds ) )
+			$resRoomIds = $ilDB->query($query);
+			while ($row = $ilDB->fetchAssoc($resRoomIds))
 			{
 				$roomsMatchingAttributeFilters [$row ['id']] = 1;
 			}
@@ -104,9 +107,9 @@ class ilRoomSharingRooms
 		if ($debug)
 		{
 			echo "<br />";
-			print_r( $roomsMatchingAttributeFilters );
+			print_r($roomsMatchingAttributeFilters);
 			echo "<br />";
-			print_r( array_keys( $roomsMatchingAttributeFilters ) );
+			print_r(array_keys($roomsMatchingAttributeFilters));
 			echo "<br />";
 		}
 		
@@ -115,14 +118,14 @@ class ilRoomSharingRooms
 		 */
 		if ($filter ["date"] && $filter ["time_from"] && $filter ["time_to"])
 		{
-			$date_from = $filter ['date'] . ' ' . $filter ['time_from'];
-			$date_to = $filter ['date'] . ' ' . $filter ['time_to'];
-			$roomsBookedInTimeRange = $this->getRoomsBookedInDateTimeRange( $date_from, $date_to );
+			$date_from = $filter ['date'].' '.$filter ['time_from'];
+			$date_to = $filter ['date'].' '.$filter ['time_to'];
+			$roomsBookedInTimeRange = $this->getRoomsBookedInDateTimeRange($date_from, $date_to);
 			$roomsMatchingAttributeFilters_Temp = $roomsMatchingAttributeFilters;
 			$roomsMatchingAttributeFilters = array ();
-			foreach( $roomsMatchingAttributeFilters_Temp as $key => $value )
+			foreach ($roomsMatchingAttributeFilters_Temp as $key => $value)
 			{
-				if (array_search( $key, $roomsBookedInTimeRange ) > - 1)
+				if (array_search($key, $roomsBookedInTimeRange) > - 1)
 				{
 				} else
 				{
@@ -134,27 +137,29 @@ class ilRoomSharingRooms
 		/*
 		 * Add remaining filters to query string
 		 */
-		$where_part = ' WHERE room.pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) . ' ';
-		$where_part = ' AND room.pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) . ' ';
+		$where_part = ' WHERE room.pool_id = '.$ilDB->quote($this->pool_id, 'integer').' ';
+		$where_part = ' AND room.pool_id = '.$ilDB->quote($this->pool_id, 'integer').' ';
 		
 		if ($filter ["room_name"] || $filter ["room_name"] === "0")
 		{
-			$where_part = $where_part . ' AND name LIKE ' . $ilDB->quote( '%' . $filter ["room_name"] . '%', 'text' ) . ' ';
+			$where_part = $where_part.' AND name LIKE '.$ilDB->quote('%'.$filter ["room_name"].'%', 'text').' ';
 		}
 		if ($filter ["room_seats"] || $filter ["room_seats"] === 0.0)
 		{
-			$where_part = $where_part . ' AND max_alloc >= ' . $ilDB->quote( $filter ["room_seats"], 'integer' ) . ' ';
+			$where_part = $where_part.' AND max_alloc >= '.$ilDB->quote($filter ["room_seats"], 'integer').' ';
 		}
 		
 		/*
 		 * Prepare and execute statement
 		 */
-		$st = $ilDB->prepare( 'SELECT room.id, name, max_alloc FROM rep_robj_xrs_rooms room WHERE ' . $ilDB->in( "room.id", array_keys( $roomsMatchingAttributeFilters ) ) . $where_part . ' ORDER BY name', $ilDB->addTypesToArray( $types, "integer", count( $room . ids ) ) );
-		$set = $ilDB->execute( $st, array_keys( $roomsMatchingAttributeFilters ) );
+		$st = $ilDB->prepare('SELECT room.id, name, max_alloc FROM rep_robj_xrs_rooms room WHERE '.
+				$ilDB->in("room.id", array_keys($roomsMatchingAttributeFilters)).$where_part.
+				' ORDER BY name', $ilDB->addTypesToArray($types, "integer", count($room.ids)));
+		$set = $ilDB->execute($st, array_keys($roomsMatchingAttributeFilters));
 		
 		$res_room = array ();
 		$room_ids = array ();
-		while ( $row = $ilDB->fetchAssoc( $set ) )
+		while ($row = $ilDB->fetchAssoc($set))
 		{
 			$res_room [] = $row;
 			$room_ids [] = $row ['id']; // Remember the ids in order to filter for room attributes within the number of room ids
@@ -163,8 +168,8 @@ class ilRoomSharingRooms
 		/*
 		 * Bring data in a specific format for display purposes
 		 */
-		$res_attribute = $this->getAttributes( $room_ids );
-		$res = $this->formatDataForGui( $res_room, $res_attribute );
+		$res_attribute = $this->getAttributes($room_ids);
+		$res = $this->formatDataForGui($res_room, $res_attribute);
 		return $res;
 	}
 	
@@ -178,10 +183,10 @@ class ilRoomSharingRooms
 	{
 		global $ilDB;
 		
-		$set = $ilDB->query( 'SELECT name FROM rep_robj_xrs_rattr ORDER BY name' );
+		$set = $ilDB->query('SELECT name FROM rep_robj_xrs_rattr ORDER BY name');
 		
 		$attributes = array ();
-		while ( $row = $ilDB->fetchAssoc( $set ) )
+		while ($row = $ilDB->fetchAssoc($set))
 		{
 			$attributes [] = $row ['name'];
 		}
@@ -200,11 +205,14 @@ class ilRoomSharingRooms
 	{
 		global $ilDB;
 		
-		$st = $ilDB->prepare( 'SELECT room_id, att.name, count FROM rep_robj_xrs_room_attr ' . ' LEFT JOIN rep_robj_xrs_rattr as att ON att.id = rep_robj_xrs_room_attr.att_id' . ' WHERE ' . $ilDB->in( "room_id", $room_ids ) . ' ORDER BY room_id, att.name', $ilDB->addTypesToArray( $types, "integer", count( $room_ids ) ) );
-		$set = $ilDB->execute( $st, $room_ids );
+		$st = $ilDB->prepare('SELECT room_id, att.name, count FROM rep_robj_xrs_room_attr '.
+				' LEFT JOIN rep_robj_xrs_rattr as att ON att.id = rep_robj_xrs_room_attr.att_id'.
+				' WHERE '.$ilDB->in("room_id", $room_ids).
+				' ORDER BY room_id, att.name', $ilDB->addTypesToArray($types, "integer", count($room_ids)));
+		$set = $ilDB->execute($st, $room_ids);
 		
 		$res_attribute = array ();
-		while ( $row = $ilDB->fetchAssoc( $set ) )
+		while ($row = $ilDB->fetchAssoc($set))
 		{
 			$res_attribute [] = $row;
 		}
@@ -235,10 +243,10 @@ class ilRoomSharingRooms
 	protected function formatDataForGui(array $res_room, array $res_attribute)
 	{
 		$res = array ();
-		foreach( $res_room as $room )
+		foreach ($res_room as $room)
 		{
 			$attr = array ();
-			foreach( $res_attribute as $attribute )
+			foreach ($res_attribute as $attribute)
 			{
 				if ($attribute ['room_id'] == $room ['id'])
 				{
@@ -264,8 +272,9 @@ class ilRoomSharingRooms
 	public function getMaxSeatCount()
 	{
 		global $ilDB;
-		$valueSet = $ilDB->query( 'SELECT MAX(max_alloc) AS value FROM rep_robj_xrs_rooms  WHERE pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) );
-		$valueRow = $ilDB->fetchAssoc( $valueSet );
+		$valueSet = $ilDB->query('SELECT MAX(max_alloc) AS value FROM rep_robj_xrs_rooms  WHERE pool_id = '.
+				$ilDB->quote($this->pool_id, 'integer'));
+		$valueRow = $ilDB->fetchAssoc($valueSet);
 		$value = $valueRow ['value'];
 		return $value;
 	}
@@ -282,14 +291,18 @@ class ilRoomSharingRooms
 	{
 		global $ilDB;
 		// get the id of the attribute in this pool
-		$attributIdSet = $ilDB->query( 'SELECT id FROM rep_robj_xrs_rattr WHERE name =' . $ilDB->quote( $a_room_attribute, 'text' ) . ' AND pool_id = ' . $ilDB->quote( $this->pool_id, 'integer' ) );
-		$attributIdRow = $ilDB->fetchAssoc( $attributIdSet );
+		$attributIdSet = $ilDB->query('SELECT id FROM rep_robj_xrs_rattr WHERE name ='.
+				$ilDB->quote($a_room_attribute, 'text').
+				' AND pool_id = '.$ilDB->quote($this->pool_id, 'integer'));
+		$attributIdRow = $ilDB->fetchAssoc($attributIdSet);
 		$attributID = $attributIdRow ['id'];
 		
 		// get the max value of the attribut in this pool
-		$valueSet = $ilDB->query( 'SELECT MAX(count) AS value FROM rep_robj_xrs_room_attr LEFT JOIN rep_robj_xrs_rooms as room
-				 ON room.id = rep_robj_xrs_room_attr.room_id WHERE att_id =' . $ilDB->quote( $attributID, 'integer' ) . ' AND pool_id =' . $ilDB->quote( $this->pool_id, 'integer' ) );
-		$valueRow = $ilDB->fetchAssoc( $valueSet );
+		$valueSet = $ilDB->query('SELECT MAX(count) AS value FROM rep_robj_xrs_room_attr LEFT JOIN rep_robj_xrs_rooms as room
+				 ON room.id = rep_robj_xrs_room_attr.room_id WHERE att_id ='.
+				$ilDB->quote($attributID, 'integer').
+				' AND pool_id ='.$ilDB->quote($this->pool_id, 'integer'));
+		$valueRow = $ilDB->fetchAssoc($valueSet);
 		$value = $valueRow ['value'];
 		return $value;
 	}
@@ -305,8 +318,9 @@ class ilRoomSharingRooms
 	public function getRoomName($a_room_id)
 	{
 		global $ilDB;
-		$roomNameSet = $ilDB->query( ' SELECT name FROM rep_robj_xrs_rooms' . ' WHERE id = ' . $ilDB->quote( $a_room_id, 'integer' ) );
-		$roomNameRow = $ilDB->fetchAssoc( $roomNameSet );
+		$roomNameSet = $ilDB->query(' SELECT name FROM rep_robj_xrs_rooms'.
+				' WHERE id = '.$ilDB->quote($a_room_id, 'integer'));
+		$roomNameRow = $ilDB->fetchAssoc($roomNameSet);
 		return $roomNameRow ['name'];
 	}
 	
@@ -327,14 +341,20 @@ class ilRoomSharingRooms
 		$roomQuery = '';
 		if ($room_id)
 		{
-			$roomQuery = ' room_id = ' . $ilDB->quote( $room_id, 'text' ) . ' AND ';
+			$roomQuery = ' room_id = '.$ilDB->quote($room_id, 'text').' AND ';
 		}
 		
-		$query = 'SELECT DISTINCT room_id FROM rep_robj_xrs_bookings WHERE ' . $roomQuery . ' (' . $ilDB->quote( $date_from, 'timestamp' ) . ' BETWEEN date_from AND date_to OR ' . $ilDB->quote( $date_to, 'timestamp' ) . ' BETWEEN date_from AND date_to OR date_from BETWEEN ' . $ilDB->quote( $date_from, 'timestamp' ) . ' AND ' . $ilDB->quote( $date_to, 'timestamp' ) . ' OR date_to BETWEEN ' . $ilDB->quote( $date_from, 'timestamp' ) . ' AND ' . $ilDB->quote( $date_to, 'timestamp' ) . ')';
+		$query = 'SELECT DISTINCT room_id FROM rep_robj_xrs_bookings WHERE '.$roomQuery.
+		' ('.$ilDB->quote($date_from, 'timestamp').' BETWEEN date_from AND date_to OR '.
+		$ilDB->quote($date_to, 'timestamp').
+		' BETWEEN date_from AND date_to OR date_from BETWEEN '.$ilDB->quote($date_from, 'timestamp').
+		' AND '.$ilDB->quote($date_to, 'timestamp').
+		' OR date_to BETWEEN '.$ilDB->quote($date_from, 'timestamp').
+		' AND '.$ilDB->quote($date_to, 'timestamp').')';
 		
-		$set = $ilDB->query( $query );
+		$set = $ilDB->query($query);
 		$res_room = array ();
-		while ( $row = $ilDB->fetchAssoc( $set ) )
+		while ($row = $ilDB->fetchAssoc($set))
 		{
 			$res_room [] = $row ['room_id'];
 		}
