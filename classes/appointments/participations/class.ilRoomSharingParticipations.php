8139ccca50b7516ad5e240ad2321350ca6281eb9
Tim Röhrig <troehrig@stud.hs-bremen.de>
2014-10-20 16:47:29 +0200

    #379 database-Pfadangabe korrigiert Nr.3

--- classes/appointments/participations/class.ilRoomSharingParticipations.php
+++ classes/appointments/participations/class.ilRoomSharingParticipations.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once '../../database/class.ilRoomSharingDatabase.php';
+include_once("Customizing/global/plugins/Services/Repository/RepositoryObject/RoomSharing/classes/database/class.ilRoomSharingDatabase.php");
 
 /**
  * Class ilRoomSharingParticipations
--- classes/rooms/detail/class.ilRoomSharingRoom.php
+++ classes/rooms/detail/class.ilRoomSharingRoom.php
@@ -1,5 +1,5 @@
 ﻿<?php
-include_once '../../database/class.ilRoomSharingDatabase.php';
+include_once("Customizing/global/plugins/Services/Repository/RepositoryObject/RoomSharing/classes/database/class.ilRoomSharingDatabase.php");
 
 /**
  * Class ilRoomSharingRoom.
