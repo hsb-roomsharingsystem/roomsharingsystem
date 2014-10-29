3fbe7cf77bc27398e2ab1fb5117f03f4b9a71d8e
Thomas Matern <tmb@live.de>
2014-10-29 12:00:10 +0100

    #384 Refactoring von Seleniumtests. - ilRoomSharingAcceptanceTestSuite gelöscht, da es mit normalen PHPUnittests kollidiert - ilRoomSharingAcceptanceGUITest refactort - configuration.xml angepasst

--- test/acceptance/tests/ilRoomSharingAcceptanceSearchTest.php
+++ test/acceptance/tests/ilRoomSharingAcceptanceSearchTest.php
@@ -4,20 +4,21 @@ include_once './acceptance/php-webdriver/__init__.php';
 
 /**
  * Example of an PHPUnit-Test with Selenium. Produces simple search test.
- *
+ * Make sure you started selenium-server-standalone-2.43.1.jar and have firefox browser on the host.
+
  * @author Thomas Matern <tmatern@stud.hs-bremen.de>
- *
- * @property WebDriver $driver
+ * @group acceptance
+ * @property WebDriver $webDriver
  */
 class ilRoomSharingAcceptanceSearchTest extends PHPUnit_Framework_TestCase
 {
-	private $driver;
+	private $webDriver;
 
 	protected function setUp()
 	{
-		/* @var $webDriver WebDriver */
-		global $webDriver;
-		$this->driver = $webDriver;
+		$host = 'http://localhost:4444/wd/hub'; // this is the default
+		$capabilities = DesiredCapabilities::firefox();
+		$this->webDriver = RemoteWebDriver::create($host, $capabilities, 5000);
 	}
 
 	/**
@@ -27,25 +28,33 @@ class ilRoomSharingAcceptanceSearchTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testExample()
 	{
-		$this->driver->get('http://google.de/');
+		$this->webDriver->get('http://google.de/');
 
 		// Find search field and search.
-		$searchField = $this->driver->findElement(WebDriverBy::name('q'));
+		$searchField = $this->webDriver->findElement(WebDriverBy::name('q'));
 		$searchField->sendKeys("Hochschule Bremen")->submit();
 
 		// Wait until the page with search results is loaded.
-		$this->driver->wait(10, 500)->until(
+		$this->webDriver->wait(10, 500)->until(
 			WebDriverExpectedCondition::presenceOfElementLocated(
 				WebDriverBy::partialLinkText('Hochschule Bremen')
 			)
 		);
 
 		// Find first search result containing search expression and click on it.
-		$firstMatch = $this->driver->findElement(WebDriverBy::partialLinkText('Hochschule Bremen'));
+		$firstMatch = $this->webDriver->findElement(WebDriverBy::partialLinkText('Hochschule Bremen'));
 		$firstMatch->click();
 
 		// Check whether we are on the right page or not.
-		$this->assertContains("Hochschule", $this->driver->getTitle());
+		$this->assertContains("Hochschule", $this->webDriver->getTitle());
+	}
+
+	/**
+	 * Closes web browser.
+	 */
+	protected function tearDown()
+	{
+		$this->webDriver->quit();
 	}
 
 }
