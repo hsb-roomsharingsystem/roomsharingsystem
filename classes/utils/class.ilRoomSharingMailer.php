e3007f98aac4341423c11fa997166c527731fe7f
Fabian Müller <fmu@never-afk.de>
2014-11-01 17:00:24 +0100

    New mail class

---
+++ classes/utils/class.ilRoomSharingMailer.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This class is used for generating mails to inform users about the bookings.
+ * The ilSystemNotification-Class does not work with our language files, so we
+ * have to implement our own mailer.
+ *
+ * @author Fabian Müller <fmu@never-afk.de>
+ * @version $Id$
+ */
+
+class ilRoomSharingMailer extends ilMailNotification
+{
+    
+    
+}
