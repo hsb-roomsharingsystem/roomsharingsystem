5ebb8e476f103bf5a4409f1d919714f5b2db99c7
Fabian Müller <fmu@never-afk.de>
2014-11-01 18:58:33 +0100

    #398 added mail notification

--- classes/utils/class.ilRoomSharingMailer.php
+++ classes/utils/class.ilRoomSharingMailer.php
@@ -7,6 +7,7 @@ include_once 'Services/Mail/classes/class.ilMailNotification.php';
  * This class is used for generating mails to inform users about the bookings.
  * The ilSystemNotification-Class does not work with our language files, so we
  * have to implement our own mailer.
+ * 
  *
  * @author Fabian Müller <fmu@never-afk.de>
  * @version $Id$
