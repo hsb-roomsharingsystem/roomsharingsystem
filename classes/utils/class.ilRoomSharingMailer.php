4ef81529403628922c9c6d752f1f427ade1f2a47
Fabian Müller <fmu@never-afk.de>
2014-11-01 17:29:37 +0100

    implemented mail functions

--- classes/utils/class.ilRoomSharingMailer.php
+++ classes/utils/class.ilRoomSharingMailer.php
@@ -14,6 +14,65 @@ include_once 'Services/Mail/classes/class.ilMailNotification.php';
 
 class ilRoomSharingMailer extends ilMailNotification
 {
+    protected $subjectraw; // string
     
+
+    /**
+     * Compose notification to single recipient
+     * 
+     * @return bool
+     */
+    public function compose()
+    {
+        $this->initMail();
+        $this->setSubject($this->subjectraw);
+        $this->setBody("Hallo!");
+        $this->appendBody("\n\n");
+        $this->appendBody("Du hast nen Raum gebucht.");
+        
+        return true;
+        
+    }
     
+    /**
+     * Send notification to single recipient
+     * 
+     * @return bool
+     */
+    protected function composeAndSendMail($a_user_id)
+    {						
+            if($this->compose())
+            {
+                    parent::sendMail(array($a_user_id), array('system'), is_numeric($a_user_id));								
+                    return true;
+            }
+            return false;
+    }
+    
+    /**
+     * Send notification(s)
+     * 
+     * @param array $a_user_ids
+     * @return array recipient ids
+     */
+    public function sendMail(array $a_user_ids) {
+
+        $recipient_ids = array();
+        foreach(array_unique($a_user_ids) as $user_id)
+        {				
+                // author of change should not get notification
+                if($this->changed_by == $user_id)
+                {
+                        continue;
+                }
+
+                if($this->composeAndSendMail($user_id))
+                {
+                        $recipient_ids[] = $user_id;
+                }
+        }	
+
+        return $recipient_ids;
+    }
+
 }
