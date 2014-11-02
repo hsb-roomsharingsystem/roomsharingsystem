1abc6a5ded07ac30831c1b6c3bf955b46b0a80b4
Thomas Matern <tmb@live.de>
2014-11-02 16:43:22 +0100

    #434: Anpassung der Konfigurations-GUI

--- classes/class.ilRoomSharingConfigGUI.php
+++ classes/class.ilRoomSharingConfigGUI.php
@@ -1,109 +1,40 @@
 <?php
 
-include_once("./Services/Component/classes/class.ilPluginConfigGUI.php");
+require_once("./Services/Component/classes/class.ilPluginConfigGUI.php");
+require_once("Services/Form/classes/class.ilPropertyFormGUI.php");
 
 /**
- * Example configuration user interface class
+ * Configuration GUI for the RoomSharing-Plugin.
  *
- * @author Alex Killing <alex.killing@gmx.de>
+ * @author Thomas Matern <tmatern@stud.hs-bremen.de>
  * @version $Id$
  *
  */
-class ilRoomSharingConfigGUI extends ilPluginConfigGUI {
-
+class ilRoomSharingConfigGUI extends ilPluginConfigGUI
+{
 	/**
 	 * Handles all commmands, default is "configure"
 	 * @param type $cmd
 	 */
-	function performCommand($cmd) 
+	function performCommand($cmd)
 	{
-
-		switch ($cmd) 
-		{
-			case "configure":
-				break;
-			case "save":
-				$this->$cmd();
-				break;
-			default:
-				break;
-		}
+		// Switch case delete, due it's not required.
+		$this->configure();
 	}
 
 	/**
-	 * Configure screen
+	 * Configuration gui.
 	 */
-	function configure() 
+	function configure()
 	{
 		global $tpl;
+		$pl = $this->getPluginObject();
 
-		$form = $this->initConfigurationForm();
+		$form = new ilPropertyFormGUI();
+		$form->setTitle($pl->txt('roomsharing_plugin_configuration'));
+		$form->setDescription($pl->txt('roomsharing_plugin_config_not_required'));
+
 		$tpl->setContent($form->getHTML());
 	}
 
-	//
-	// From here on, this is just an example implementation using
-	// a standard form (without saving anything)
-
-	//
-	
-	/**
-	 * Init configuration form.
-	 *
-	 * @return object form object
-	 */
-	public function initConfigurationForm() 
-	{
-		global $lng, $ilCtrl;
-
-		$pl = $this->getPluginObject();
-
-		include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
-		$form = new ilPropertyFormGUI();
-
-		// setting 1 (a checkbox)
-		$cb = new ilCheckboxInputGUI($pl->txt("setting_1"), "setting_1");
-		$form->addItem($cb);
-
-		// setting 2 (text)
-		$ti = new ilTextInputGUI($pl->txt("setting_2"), "setting_2");
-		$ti->setRequired(true);
-		$ti->setMaxLength(10);
-		$ti->setSize(10);
-		$form->addItem($ti);
-
-		$form->addCommandButton("save", $lng->txt("save"));
-
-		$form->setTitle($pl->txt("roomsharing_plugin_configuration"));
-		$form->setFormAction($ilCtrl->getFormAction($this));
-
-		return $form;
-	}
-
-	/**
-	 * Save form input (currently does not save anything to db)
-	 *
-	 */
-	public function save() 
-	{
-		global $tpl, $lng, $ilCtrl;
-
-		$pl = $this->getPluginObject();
-
-		$form = $this->initConfigurationForm();
-		if ($form->checkInput()) 
-			{
-			$set1 = $form->getInput("setting_1");
-			$set2 = $form->getInput("setting_2");
-
-			ilUtil::sendSuccess($pl->txt("rep_robj_xrs_saving_invoked"), true);
-			$ilCtrl->redirect($this, "configure");
-			} else 
-			{
-			$form->setValuesByPost();
-			$tpl->setContent($form->getHtml());
-			}
-	}
-
 }
-
