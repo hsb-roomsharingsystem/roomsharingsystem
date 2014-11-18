57de461bb13d4407f2d6dcc7c0554ae7a02d5030
Robert Heimsoth <rheimsoth@stud.hs-bremen.de>
2014-11-18 18:31:24 +0100

    #446 Rechteberücksichtigung bei Privilegien eingebaut

--- classes/privileges/class.ilRoomSharingClassPrivilegesTableGUI.php
+++ classes/privileges/class.ilRoomSharingClassPrivilegesTableGUI.php
@@ -14,169 +14,188 @@ require_once("./Services/UIComponent/Tooltip/classes/class.ilTooltipGUI.php");
  */
 class ilRoomSharingClassPrivilegesTableGUI extends ilTable2GUI
 {
-    private $ctrl;
-    private $privileges;
-    private $ref_id;
+	private $ctrl;
+	private $privileges;
+	private $ref_id;
+	private $permission;
 
-    /**
-     * Constructor of ilRoomSharingClassPrivilegesTableGUI
-     * @return
-     */
-    public function __construct($a_parent_obj, $a_parent_cmd, $a_ref_id = 1)
-    {
-        global $ilCtrl, $lng;
+	/**
+	 * Constructor of ilRoomSharingClassPrivilegesTableGUI
+	 * @return
+	 */
+	public function __construct($a_parent_obj, $a_parent_cmd, $a_ref_id = 1)
+	{
+		global $ilCtrl, $lng, $rssPermission;
 
-        $this->lng = $lng;
-        $this->ctrl = $ilCtrl;
-        $this->ref_id = $a_ref_id;
+		$this->lng = $lng;
+		$this->ctrl = $ilCtrl;
+		$this->ref_id = $a_ref_id;
+		$this->permission = $rssPermission;
 
-        $this->privileges = new ilRoomSharingPrivileges($a_parent_obj->getPoolId());
+		$this->privileges = new ilRoomSharingPrivileges($a_parent_obj->getPoolId());
 
-        parent::__construct($a_parent_obj, $a_parent_cmd);
+		parent::__construct($a_parent_obj, $a_parent_cmd);
 
-        $this->setId('class_priv_' . $this->ref_id);
-        $this->setTitle($this->lng->txt("rep_robj_xrs_privileges_settings"));
-        $this->setEnableHeader(true);
-        $this->setFormAction($ilCtrl->getFormAction($a_parent_obj, $a_parent_cmd));
-        $this->setLimit(100);
-        $this->setShowRowsSelector(false);
-        $this->addCommandButton('savePrivileges', $this->lng->txt('save'));
+		$this->setId('class_priv_' . $this->ref_id);
+		$this->setTitle($this->lng->txt("rep_robj_xrs_privileges_settings"));
+		$this->setEnableHeader(true);
+		$this->setFormAction($ilCtrl->getFormAction($a_parent_obj, $a_parent_cmd));
+		$this->setLimit(100);
+		$this->setShowRowsSelector(false);
+		if ($this->permission->checkPrivilege("editPrivileges"))
+		{
+			$this->addCommandButton('savePrivileges', $this->lng->txt('save'));
+		}
 
-        $this->addColumns();
-        $this->populateTable();
-    }
+		$this->addColumns();
+		$this->populateTable();
+	}
 
-    private function populateTable()
-    {
-        global $tpl;
+	private function populateTable()
+	{
+		global $tpl;
 
-        $this->setRowTemplate("tpl.room_class_privileges_row.html", "Customizing/global/plugins/Services/Repository/RepositoryObject/RoomSharing/");
-        $tpl->addJavaScript("Customizing/global/plugins/Services/Repository/RepositoryObject/RoomSharing/js/ilPrivilegesSelect.js");
+		$this->setRowTemplate("tpl.room_class_privileges_row.html",
+			"Customizing/global/plugins/Services/Repository/RepositoryObject/RoomSharing/");
+		$tpl->addJavaScript("Customizing/global/plugins/Services/Repository/RepositoryObject/RoomSharing/js/ilPrivilegesSelect.js");
 
-        $data = $this->privileges->getPrivilegesMatrix();
+		$data = $this->privileges->getPrivilegesMatrix();
 
-        $this->setMaxCount(count($data));
-        $this->setData($data);
-    }
+		$this->setMaxCount(count($data));
+		$this->setData($data);
+	}
 
-    private function addColumns()
-    {
-        $classes = $this->privileges->getClasses();
+	private function addColumns()
+	{
+		$classes = $this->privileges->getClasses();
 
-        foreach ($classes as $class)
-        {
-            $this->addColumn($this->createTitle($class), "", "", "", false);
-        }
-    }
+		foreach ($classes as $class)
+		{
+			$this->addColumn($this->createTitle($class), "", "", "", false);
+		}
+	}
 
-    public function fillHeader()
-    {
-        parent::fillHeader();
-        $this->addTooltips();
-    }
+	public function fillHeader()
+	{
+		parent::fillHeader();
+		$this->addTooltips();
+	}
 
-    public function fillRow($a_table_row)
-    {
-        // Lock Class
-        if (isset($a_table_row["show_lock_row"]))
-        {
-            $classes = $this->privileges->getClasses();
-            foreach ($classes as $class)
-            {
-                $this->tpl->setCurrentBlock("class_lock");
-                $this->tpl->setVariable("LOCK_CLASS_ID", $class["id"]);
-                $this->tpl->setVariable("TXT_LOCK", $this->lng->txt("rep_robj_xrs_privileges_lock"));
-                $this->tpl->setVariable("TXT_LOCK_LONG", $this->lng->txt("rep_robj_xrs_privileges_lock_desc"));
+	public function fillRow($a_table_row)
+	{
+		// Lock Class
+		if (isset($a_table_row["show_lock_row"]))
+		{
+			$classes = $this->privileges->getClasses();
+			foreach ($classes as $class)
+			{
+				$this->tpl->setCurrentBlock("class_lock");
+				$this->tpl->setVariable("LOCK_CLASS_ID", $class["id"]);
+				$this->tpl->setVariable("TXT_LOCK", $this->lng->txt("rep_robj_xrs_privileges_lock"));
+				$this->tpl->setVariable("TXT_LOCK_LONG", $this->lng->txt("rep_robj_xrs_privileges_lock_desc"));
 
-                if (in_array($class["id"], $a_table_row["locked_classes"]))
-                {
-                    $this->tpl->setVariable("LOCK_CHECKED", "checked='checked'");
-                }
+				if (in_array($class["id"], $a_table_row["locked_classes"]))
+				{
+					$this->tpl->setVariable("LOCK_CHECKED", "checked='checked'");
+				}
 
-                $this->tpl->parseCurrentBlock();
-            }
-            return true;
-        }
+				$this->tpl->parseCurrentBlock();
+			}
+			return true;
+		}
 
-        // Section info
-        if (isset($a_table_row["show_section_info"]))
-        {
-            $this->tpl->setCurrentBlock("section_info");
-            $this->tpl->setVariable("SECTION_TITLE", $a_table_row["section"]["title"]);
-            $this->tpl->setVariable("SECTION_DESC", $a_table_row["section"]["description"]);
-            $this->tpl->parseCurrentBlock();
+		// Section info
+		if (isset($a_table_row["show_section_info"]))
+		{
+			$this->tpl->setCurrentBlock("section_info");
+			$this->tpl->setVariable("SECTION_TITLE", $a_table_row["section"]["title"]);
+			$this->tpl->setVariable("SECTION_DESC", $a_table_row["section"]["description"]);
+			$this->tpl->parseCurrentBlock();
 
-            return true;
-        }
+			return true;
+		}
 
-        // Select all
-        if (isset($a_table_row['show_select_all']))
-        {
-            $classes = $this->privileges->getClasses();
+		// Select all
+		if (isset($a_table_row['show_select_all']))
+		{
+			$classes = $this->privileges->getClasses();
 
-            foreach ($classes as $class)
-            {
-                $this->tpl->setCurrentBlock("class_select_all");
-                $this->tpl->setVariable("JS_CLASS_ID", $class["id"]);
-                $this->tpl->setVariable("JS_FORM_NAME", $this->getFormName());
-                $this->tpl->setVariable("JS_SUBID", $a_table_row["type"]);
-                $this->tpl->setVariable("JS_ALL_PRIVS", "['" . implode("','", $a_table_row["privileges"]) . "']");
-                $this->tpl->setVariable("TXT_SEL_ALL", $this->lng->txt("select_all"));
-                $this->tpl->parseCurrentBlock();
-            }
-            return true;
-        }
+			foreach ($classes as $class)
+			{
+				$this->tpl->setCurrentBlock("class_select_all");
+				$this->tpl->setVariable("JS_CLASS_ID", $class["id"]);
+				$this->tpl->setVariable("JS_FORM_NAME", $this->getFormName());
+				$this->tpl->setVariable("JS_SUBID", $a_table_row["type"]);
+				$this->tpl->setVariable("JS_ALL_PRIVS", "['" . implode("','", $a_table_row["privileges"]) . "']");
+				$this->tpl->setVariable("TXT_SEL_ALL", $this->lng->txt("select_all"));
+				if (!$this->permission->checkPrivilege("editPrivileges"))
+				{
+					$this->tpl->setVariable("PRIV_DISABLED", 'disabled="disabled"');
+				}
+				$this->tpl->parseCurrentBlock();
+			}
+			return true;
+		}
 
-        // Privileges
-        foreach ($a_table_row["classes"] as $class)
-        {
-            $this->tpl->setCurrentBlock("class_td");
-            $this->tpl->setVariable("PRIV_CLASS_ID", $class["id"]);
-            $this->tpl->setVariable("PRIV_ID", $a_table_row["privilege"]["id"]);
+		// Privileges
+		foreach ($a_table_row["classes"] as $class)
+		{
+			$this->tpl->setCurrentBlock("class_td");
+			$this->tpl->setVariable("PRIV_CLASS_ID", $class["id"]);
+			$this->tpl->setVariable("PRIV_ID", $a_table_row["privilege"]["id"]);
 
-            $this->tpl->setVariable("TXT_PRIV", $a_table_row["privilege"]["name"]);
+			$this->tpl->setVariable("TXT_PRIV", $a_table_row["privilege"]["name"]);
 
-            $this->tpl->setVariable("TXT_PRIV_LONG", $a_table_row["privilege"]["description"]);
+			$this->tpl->setVariable("TXT_PRIV_LONG", $a_table_row["privilege"]["description"]);
 
-            if ($class["privilege_set"])
-            {
-                $this->tpl->setVariable("PRIV_CHECKED", 'checked="checked"');
-            }
+			if ($class["privilege_set"])
+			{
+				$this->tpl->setVariable("PRIV_CHECKED", 'checked="checked"');
+			}
 
-            $this->tpl->parseCurrentBlock();
-        }
-    }
+			if (!$this->permission->checkPrivilege("editPrivileges"))
+			{
+				$this->tpl->setVariable("PRIV_DISABLED", 'disabled="disabled"');
+			}
 
-    private function addTooltips()
-    {
-        $classes = $this->privileges->getClasses();
+			$this->tpl->parseCurrentBlock();
+		}
+	}
 
-        $cnt = 1;
-        foreach ($classes as $class)
-        {
-            $role_text = $this->isClassAssignedToRole($class) ? $class["role"] : $this->lng->txt("none");
+	private function addTooltips()
+	{
+		$classes = $this->privileges->getClasses();
 
-            ilTooltipGUI::addTooltip("thc_" . $this->getId() . "_" . $cnt, "<pre>" . $this->lng->txt("rep_robj_xrs_class") . ": " . $class["name"] . "&#13;&#10;"
-                . $this->lng->txt("rep_robj_xrs_privileges_role_assignment") . ": " . $role_text . "&#13;&#10;" . $this->lng->txt("rep_robj_xrs_class_priority") . ": " . $class["priority"] . "</pre>", "", "bottom center", "top center", false);
-            $cnt++;
-        }
-    }
+		$cnt = 1;
+		foreach ($classes as $class)
+		{
+			$role_text = $this->isClassAssignedToRole($class) ? $class["role"] : $this->lng->txt("none");
 
-    private function createTitle($a_class_set)
-    {
-        // &#8658; = Unicode double arrow to the right
-        $assigned_role = $this->isClassAssignedToRole($a_class_set) ? " &#8658; " . $a_class_set["role"] : null;
-        $table_head = $a_class_set["name"] . $assigned_role;
+			ilTooltipGUI::addTooltip("thc_" . $this->getId() . "_" . $cnt,
+				"<pre>" . $this->lng->txt("rep_robj_xrs_class") . ": " . $class["name"] . "&#13;&#10;"
+				. $this->lng->txt("rep_robj_xrs_privileges_role_assignment") . ": " . $role_text . "&#13;&#10;" . $this->lng->txt("rep_robj_xrs_class_priority") . ": " . $class["priority"] . "</pre>",
+				"", "bottom center", "top center", false);
+			$cnt++;
+		}
+	}
 
-        $this->ctrl->setParameterByClass("ilroomsharingclassgui", "class_id", $a_class_set["id"]);
+	private function createTitle($a_class_set)
+	{
+		// &#8658; = Unicode double arrow to the right
+		$assigned_role = $this->isClassAssignedToRole($a_class_set) ? " &#8658; " . $a_class_set["role"] : null;
+		$table_head = $a_class_set["name"] . $assigned_role;
 
-        return '<a class="tblheader" href="' . $this->ctrl->getLinkTargetByClass("ilroomsharingclassgui", "") . '" >' . $table_head . "</a>";
-    }
+		$this->ctrl->setParameterByClass("ilroomsharingclassgui", "class_id", $a_class_set["id"]);
 
-    private function isClassAssignedToRole($a_class_set)
-    {
-        return !empty($a_class_set["role"]);
-    }
+		return '<a class="tblheader" href="' . $this->ctrl->getLinkTargetByClass("ilroomsharingclassgui",
+				"") . '" >' . $table_head . "</a>";
+	}
+
+	private function isClassAssignedToRole($a_class_set)
+	{
+		return !empty($a_class_set["role"]);
+	}
 
 }
+
 ?>
