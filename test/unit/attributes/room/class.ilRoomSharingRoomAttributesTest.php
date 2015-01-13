3ccd3e3b0b5ddc1175cf559434884be96c6a339b
Thomas Matern <tmb@live.de>
2015-01-13 13:18:21 +0100

    #540 Unit Tests Attributes-Room Review

--- test/unit/attributes/room/class.ilRoomSharingRoomAttributesTest.php
+++ test/unit/attributes/room/class.ilRoomSharingRoomAttributesTest.php
@@ -33,8 +33,8 @@ class ilRoomSharingRoomAttributesTest extends PHPUnit_Framework_TestCase
 				->disableOriginalConstructor()->getMock();
 
 		$attributes = Array(
-			0 => Array("id" => 8, "name" => test, "pool_id" => 1),
-			1 => Array("id" => 9, "name" => test2, "pool_id" => 1));
+			0 => Array("id" => 8, "name" => "test", "pool_id" => 1),
+			1 => Array("id" => 9, "name" => "test2", "pool_id" => 1));
 
 		self::$ilRoomSharingDatabaseStub->method("getAllRoomAttributes")->willReturn($attributes);
 		self::$ilRoomSharingDatabaseStub->method("deleteAttributeRoomAssign")->willReturn(2);
