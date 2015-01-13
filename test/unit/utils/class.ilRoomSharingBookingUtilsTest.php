dfb14b8cf5073420f2b6ff247af73821726feef4
Thomas Matern <tmb@live.de>
2015-01-13 16:06:15 +0100

    #546 Review

--- test/unit/utils/class.ilRoomSharingBookingUtilsTest.php
+++ test/unit/utils/class.ilRoomSharingBookingUtilsTest.php
@@ -29,44 +29,44 @@ class ilRoomSharingBookingUtilsTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testReadBookingDate()
 	{
-		$bookingData = array(
+		$bookingData1 = array(
 			"date_from" => "2010-12-31 14:00:00",
 			"date_to" => "2010-12-31 15:00:00"
 		);
-		$expected = "test., 31. test 2010, 14:00 - 15:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected1 = "test., 31. test 2010, 14:00 - 15:00";
+		$actual1 = UTILS::readBookingDate($bookingData1);
+		$this->assertEquals($expected1, $actual1);
 
 
 
-		$bookingData = array(
+		$bookingData2 = array(
 			"date_from" => "2015-02-29 15:00:00",
 			"date_to" => "2015-02-29 16:00:00"
 		);
-		$expected = "test., 01. test 2015, 15:00 - 16:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected2 = "test., 01. test 2015, 15:00 - 16:00";
+		$actual2 = UTILS::readBookingDate($bookingData2);
+		$this->assertEquals($expected2, $actual2);
 
 
 
-		$bookingData = array(
+		$bookingData3 = array(
 			"date_from" => "2016-02-29 16:00:00",
 			"date_to" => "2016-02-29 17:00:00"
 		);
-		$expected = "test., 29. test 2016, 16:00 - 17:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected3 = "test., 29. test 2016, 16:00 - 17:00";
+		$actual3 = UTILS::readBookingDate($bookingData3);
+		$this->assertEquals($expected3, $actual3);
 
 
 
 
-		$bookingData = array(
+		$bookingData4 = array(
 			"date_from" => "2015-02-29 17:00:00",
 			"date_to" => "2015-03-01 18:00:00"
 		);
-		$expected = "test., 01. test 2015, 17:00 - 18:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected4 = "test., 01. test 2015, 17:00 - 18:00";
+		$actual4 = UTILS::readBookingDate($bookingData4);
+		$this->assertEquals($expected4, $actual4);
 	}
 
 	/**
@@ -74,33 +74,33 @@ class ilRoomSharingBookingUtilsTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testReadBookingDateMultiDate()
 	{
-		$bookingData = array(
+		$bookingData1 = array(
 			"date_from" => "2010-12-31 14:00:00",
 			"date_to" => "2011-01-03 15:00:00"
 		);
-		$expected = "test., 31. test 2010, 14:00 - <br>test., 03. test 2011, 15:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected1 = "test., 31. test 2010, 14:00 - <br>test., 03. test 2011, 15:00";
+		$actual1 = UTILS::readBookingDate($bookingData1);
+		$this->assertEquals($expected1, $actual1);
 
 
 
-		$bookingData = array(
+		$bookingData2 = array(
 			"date_from" => "2015-02-29 15:00:00",
 			"date_to" => "2015-03-02 16:00:00"
 		);
-		$expected = "test., 01. test 2015, 15:00 - <br>test., 02. test 2015, 16:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected2 = "test., 01. test 2015, 15:00 - <br>test., 02. test 2015, 16:00";
+		$actual2 = UTILS::readBookingDate($bookingData2);
+		$this->assertEquals($expected2, $actual2);
 
 
 
-		$bookingData = array(
+		$bookingData3 = array(
 			"date_from" => "2016-02-29 16:00:00",
 			"date_to" => "2016-03-02 17:00:00"
 		);
-		$expected = "test., 29. test 2016, 16:00 - <br>test., 02. test 2016, 17:00";
-		$actual = UTILS::readBookingDate($bookingData);
-		$this->assertEquals($expected, $actual);
+		$expected3 = "test., 29. test 2016, 16:00 - <br>test., 02. test 2016, 17:00";
+		$actual3 = UTILS::readBookingDate($bookingData3);
+		$this->assertEquals($expected3, $actual3);
 	}
 
 }
